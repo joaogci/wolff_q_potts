6080a27659ba021af81157185df3d4338f4cd4d9
João Inácio <jgci2000@hotmail.com>
2022-04-12 12:01:32 +0200

    2D implementation testing

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@
 
 int main(int argc, char **argv) {
     int q = 3;
-    int L = 32;
+    int L = 16;
+    int dim = 2;
 
-    WOLFF wolff(q, L, L);
+    WOLFF wolff(q, L, L * L, dim);
 
-    double T_vals[] = {0, 0.25, 0.5, 1.0, 2.0, 4.0};
-    for (int t_idx = 0; t_idx < 6; t_idx++) {
+    double T_vals[] = {0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0};
+    for (int t_idx = 0; t_idx < 9; t_idx++) {
         wolff.simulate(T_vals[t_idx], 1e5, 1e6, 1);
-        wolff.print_results();
-        wolff.write_results("../", "1D_L32_q3_potts.csv");
+        // wolff.print_results();
+        wolff.write_results("../", "2D_L16_q3_potts_pt.csv");
+        printf("t_idx = %i \n", t_idx);
     }
 
     return 0;
