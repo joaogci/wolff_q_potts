327646e3dd73e38a979147093e488862c31a3e4b
João Inácio <jgci2000@hotmail.com>
2022-04-26 16:51:40 +0200

    final results

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ using std::to_string;
 
 int main(int argc, char **argv) {
     int q = 3;
-    int L = 16;
-    int dim = 1;
+    int L = 32;
+    int dim = 2;
     int N = pow(L, dim);
     string file_name = to_string(dim) + "D_L" + to_string(L) + "_q" + to_string(q) + "_potts.csv";
 
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
     for (int t_idx = 0; t_idx < size_T; t_idx++) {
         T_vals[t_idx] = (T_e - T_s) * (t_idx + 1) / size_T;
 
-        wolff.simulate(T_vals[t_idx], 1e5, 1e5, 10);
+        wolff.simulate(T_vals[t_idx], 1e5, 1e6, 10);
         wolff.write_results("../", file_name);
         printf("t_idx = %i \n", t_idx);
     }
